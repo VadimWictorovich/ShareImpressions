6497c0fb4ba104fa1b75932e13ac9b40b31fef44
Vadim <91436203+VadimWictorovich@users.noreply.github.com>
2024-05-14 00:45:51 +0300

    Небольшая работа с констреинтами и UI элементами

--- ShareImpressions/ShareImpressions/View/AuthorizationVC.swift
+++ ShareImpressions/ShareImpressions/View/AuthorizationVC.swift
@@ -37,14 +37,20 @@ final class AuthorizationVC: UIViewController {
     private lazy var loginTF: UITextField = {
         let tf = UITextField()
         tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Введите логин"
+        tf.placeholder = "  Введите логин"
+        tf.layer.cornerRadius = 10
+        tf.layer.borderWidth = 1.0
+        tf.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         return tf
     }()
     
     private lazy var passwordTF: UITextField = {
         let tf = UITextField()
         tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Введите пароль"
+        tf.placeholder = "  Введите пароль"
+        tf.layer.cornerRadius = 10
+        tf.layer.borderWidth = 1.0
+        tf.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         return tf
     }()
     
@@ -54,6 +60,9 @@ final class AuthorizationVC: UIViewController {
         but.translatesAutoresizingMaskIntoConstraints = false
         but.setTitle("Войти", for: .normal)
         but.addTarget(nil, action: #selector(goToStartVC), for: .touchUpInside)
+        but.layer.cornerRadius = 10
+        but.layer.borderWidth = 2.0
+        but.layer.borderColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         return but
     }()
     
@@ -62,12 +71,17 @@ final class AuthorizationVC: UIViewController {
         but.translatesAutoresizingMaskIntoConstraints = false
         but.setTitle("Регистрация", for: .normal)
         but.addTarget(nil, action: #selector(goToRegisterVC), for: .touchUpInside)
+        but.layer.cornerRadius = 10
+        but.layer.borderWidth = 2.0
+        but.layer.borderColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         return but
     }()
     
     // TODO: здесь все обдумать и доделать как что будет работать
     private lazy var faceIDButton: UIButton = {
         let but = UIButton()
+        but.translatesAutoresizingMaskIntoConstraints = false
+        but.backgroundColor = .red
         return but
     }()
     
@@ -76,7 +90,6 @@ final class AuthorizationVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        view.backgroundColor = .blue
     }
     
     // MARK: - Methods
@@ -93,17 +106,44 @@ final class AuthorizationVC: UIViewController {
     
     // TODO: расставить констреинты позже!!!
     private func setupNSLayoutConstraint() {
+        NSLayoutConstraint(item: welcomeLabel, attribute: .top, relatedBy: .equal, toItem: view, attribute: .topMargin, multiplier: 1.0, constant: 70).isActive = true
+        NSLayoutConstraint(item: welcomeLabel, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -20).isActive = true
+        NSLayoutConstraint(item: welcomeLabel, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 20).isActive = true
         
+        NSLayoutConstraint(item: faceIDButton, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottomMargin, multiplier: 1.0, constant: -80).isActive = true
+        NSLayoutConstraint(item: faceIDButton, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerXWithinMargins, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: faceIDButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1.0, constant: 40).isActive = true
+        NSLayoutConstraint(item: faceIDButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 40).isActive = true
+        
+        
+        NSLayoutConstraint(item: signUpButton, attribute: .bottom, relatedBy: .equal, toItem: faceIDButton, attribute: .bottomMargin, multiplier: 1.0, constant: -70).isActive = true
+        NSLayoutConstraint(item: signUpButton, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 70).isActive = true
+        NSLayoutConstraint(item: signUpButton, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -70).isActive = true
+        
+        NSLayoutConstraint(item: signInButton, attribute: .bottom, relatedBy: .equal, toItem: signUpButton, attribute: .bottomMargin, multiplier: 1.0, constant: -40).isActive = true
+        NSLayoutConstraint(item: signInButton, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 70).isActive = true
+        NSLayoutConstraint(item: signInButton, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -70).isActive = true
+        
+        NSLayoutConstraint(item: passwordTF, attribute: .bottom, relatedBy: .equal, toItem: signInButton, attribute: .bottomMargin, multiplier: 1.0, constant: -90).isActive = true
+        NSLayoutConstraint(item: passwordTF, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 40).isActive = true
+        NSLayoutConstraint(item: passwordTF, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -40).isActive = true
+        NSLayoutConstraint(item: passwordTF, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 50).isActive = true
+        
+        NSLayoutConstraint(item: loginTF, attribute: .bottom, relatedBy: .equal, toItem: passwordTF, attribute: .bottomMargin, multiplier: 1.0, constant: -50).isActive = true
+        NSLayoutConstraint(item: loginTF, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 40).isActive = true
+        NSLayoutConstraint(item: loginTF, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -40).isActive = true
+        NSLayoutConstraint(item: loginTF, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 50).isActive = true
     }
     
     // TODO: доделать обработку нажатия
     @objc private func goToStartVC() {
-        
+        let vc = HomeVC()
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     // TODO: аналогично!!!!
     @objc private func goToRegisterVC() {
-        
+        let vc = SignUpVC()
+        navigationController?.pushViewController(vc, animated: true)
     }
-    
 }
--- ShareImpressions/ShareImpressions/View/SignUpVC.swift
+++ ShareImpressions/ShareImpressions/View/SignUpVC.swift
@@ -13,27 +13,48 @@ final class SignUpVC: UIViewController {
         let lab = UILabel()
         lab.translatesAutoresizingMaskIntoConstraints = false
         lab.text = "Пройдите регистрацию"
+        lab.textAlignment = .center
+        lab.numberOfLines = 0
         return lab
     }()
     
     private lazy var nameTF: UITextField = {
         let tf = UITextField()
         tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Введите свое имя"
+        tf.placeholder = "  Введите свое имя"
+        tf.layer.cornerRadius = 10
+        tf.layer.borderWidth = 1.0
+        tf.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         return tf
     }()
     
     private lazy var usernameTF: UITextField = {
         let tf = UITextField()
         tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Введите свой логин"
+        tf.placeholder = "  Введите свой логин"
+        tf.layer.cornerRadius = 10
+        tf.layer.borderWidth = 1.0
+        tf.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         return tf
     }()
     
     private lazy var passwordTF: UITextField = {
         let tf = UITextField()
         tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Введите пароль"
+        tf.placeholder = "  Введите пароль"
+        tf.layer.cornerRadius = 10
+        tf.layer.borderWidth = 1.0
+        tf.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        return tf
+    }()
+    
+    private lazy var dualPasswordTF: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "  Повторите пароль"
+        tf.layer.cornerRadius = 10
+        tf.layer.borderWidth = 1.0
+        tf.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         return tf
     }()
     
@@ -42,13 +63,16 @@ final class SignUpVC: UIViewController {
         but.translatesAutoresizingMaskIntoConstraints = false
         but.setTitle("Регистрация", for: .normal)
         but.addTarget(nil, action: #selector(registorAtcion), for: .touchUpInside)
+        but.layer.cornerRadius = 10
+        but.layer.borderWidth = 2.0
+        but.layer.borderColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         return but
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1)
         setupUI()
-
     }
     
     private func setupUI() {
@@ -56,10 +80,42 @@ final class SignUpVC: UIViewController {
         view.addSubview(nameTF)
         view.addSubview(usernameTF)
         view.addSubview(passwordTF)
+        view.addSubview(dualPasswordTF)
         view.addSubview(signUpButton)
+        setupNSLayoutConstraint()
+    }
+    
+    private func setupNSLayoutConstraint() {
+        NSLayoutConstraint(item: infoLabel, attribute: .top, relatedBy: .equal, toItem: view, attribute: .topMargin, multiplier: 1.0, constant: 30).isActive = true
+        NSLayoutConstraint(item: infoLabel, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -20).isActive = true
+        NSLayoutConstraint(item: infoLabel, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 20).isActive = true
+        
+        NSLayoutConstraint(item: nameTF, attribute: .top, relatedBy: .equal, toItem: infoLabel, attribute: .topMargin, multiplier: 1.0, constant: 80).isActive = true
+        NSLayoutConstraint(item: nameTF, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 40).isActive = true
+        NSLayoutConstraint(item: nameTF, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -40).isActive = true
+        NSLayoutConstraint(item: nameTF, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 50).isActive = true
+        
+        NSLayoutConstraint(item: usernameTF, attribute: .top, relatedBy: .equal, toItem: nameTF, attribute: .topMargin, multiplier: 1.0, constant: 55).isActive = true
+        NSLayoutConstraint(item: usernameTF, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 40).isActive = true
+        NSLayoutConstraint(item: usernameTF, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -40).isActive = true
+        NSLayoutConstraint(item: usernameTF, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 50).isActive = true
+        
+        NSLayoutConstraint(item: passwordTF, attribute: .top, relatedBy: .equal, toItem: usernameTF, attribute: .topMargin, multiplier: 1.0, constant: 55).isActive = true
+        NSLayoutConstraint(item: passwordTF, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 40).isActive = true
+        NSLayoutConstraint(item: passwordTF, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -40).isActive = true
+        NSLayoutConstraint(item: passwordTF, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 50).isActive = true
+        
+        NSLayoutConstraint(item: dualPasswordTF, attribute: .top, relatedBy: .equal, toItem: passwordTF, attribute: .topMargin, multiplier: 1.0, constant: 55).isActive = true
+        NSLayoutConstraint(item: dualPasswordTF, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 40).isActive = true
+        NSLayoutConstraint(item: dualPasswordTF, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -40).isActive = true
+        NSLayoutConstraint(item: dualPasswordTF, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 50).isActive = true
+        
+        NSLayoutConstraint(item: signUpButton, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottomMargin, multiplier: 1.0, constant: -40).isActive = true
+        NSLayoutConstraint(item: signUpButton, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 70).isActive = true
+        NSLayoutConstraint(item: signUpButton, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: -70).isActive = true
     }
     
     @objc private func registorAtcion() {
-        
+        navigationController?.popViewController(animated: true)
     }
 }
